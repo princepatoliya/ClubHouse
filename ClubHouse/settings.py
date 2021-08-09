792c88084b8d86ea00e5744a926564ef1b764654
Prince Patoliya <princepatoliya852@gmail.com>
2021-08-09 22:49:50 +0530

    Update settings.py

--- ClubHouse/settings.py
+++ ClubHouse/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wn*a-_%ez3dwski1&5!$$&q%q!vddt$sl$jp(76k+sputjyr)k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
